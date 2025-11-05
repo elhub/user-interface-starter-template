23e504f6a527e37bd68b4f5396363f53486b76ba
Pål Oskar Engen <pal.engen@statnett.no>
2025-11-05 16:52:18 +0100

    chore: add gh-pages scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/user-interface-starter-template/",
   plugins: [react()],
   resolve: {
     alias: {
